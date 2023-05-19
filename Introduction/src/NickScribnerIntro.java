8c112e9b63af831dd67003bbcc43893fdfff502a
ChefKirby51 <ilikepieguy11@gmail.com>
2023-05-19 13:39:40 -0400

    Lab 1 Introduction
    This is my Introduction to the class

---
+++ Introduction/src/NickScribnerIntro.java
@@ -0,0 +1,15 @@
+public class NickScribnerIntro
+{
+        public static void main(String[] args)
+        {
+            System.out.println("Hello, my name is Nick Scribner (Male) and I am taking the CIS-2353 Data Structures class for ");
+            System.out.println("Summer 2023. I am in this class because it is part of the Degree that I am working towards.");
+            System.out.println("I have some experience with Java but struggled with it. Hopefully I will be able to ");
+            System.out.println("learn more about Java in this new class.");
+            System.out.println(" ");
+            System.out.println("I mostly play video games and help out with my Church (Oakland Woods Baptist Church).");
+            System.out.println("Fun Fact: I have double jointed fingers, meaning when I stretch my fingers they go upward, ");
+            System.out.println("I cannot keep my fingers straight. They say it helps with typing and pianos but, having this ");
+            System.out.println("all my life I can't really tell the difference, obviously.");
+        }
+}
